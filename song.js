bcd5aa610aadb70a8129ec19a0837aef5df5185b
miwanoff <marina.iva.noffa@gmail.com>
2024-10-12 15:34:12 +0300

    Add Song

---
+++ song.js
@@ -0,0 +1,8 @@
+let count = 100;
+document.write("<h2>Song about " + count + "bottles of beer</h2>");
+document.write("1 bottle of beer on the wall");
+for (let i = 0; i < count; i++) {}
+
+document.write(
+  "<br>No more bottles of beer on the wall, no more bottles of beer.There's nothing else to fall, because there's no more bottles of beer on the wall."
+);
